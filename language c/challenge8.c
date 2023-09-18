065cb91d62645772208acdbe36857081f747b98d
yassir rachad <yassirrachad1997@gmail.com>
2023-09-18 12:10:44 +0100

    challenges modifier

--- language c/challenge8.c
+++ language c/challenge8.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main(){
 	//les variables:
-	int vx;
+	int vd;
 	//input:
 	printf("la valeur entier: ");
-	scanf("%d",&vx);
-	printf("valeur hexadecimal: %x \n",vx);
-	printf("valeur octal: %o",vx);
+	scanf("%d",&vd);
+	printf("valeur hexadecimal: %x \n",vd);
+	printf("valeur octal: %o",vd);
 	return 0;
 	
 }
--- language c3/challenge3.c
+++ language c3/challenge3.c
@@ -5,22 +5,32 @@ int i;
 int a=0;
 printf("donner un nombre: ");
 scanf("%d",&num);
-for(i=2;i<num;i++){
+   if (num<=1)
+    {
+        printf("Le nombre n'est premier");
+
+
+    }
+    else{
+	
+    
+for(i=2;i<num/2;i++){
 
  if(num%i==0){
 			a++;
 		}
 	}		 if(a==0){
-				printf("le nombre est premier\n");
+				printf("le nombre est premier");
 				
 			}
 			
 			else{
-				printf("le nombre est ne pas premier\n");
+				printf("le nombre est ne pas premier");
 				
 			}
 			
 		}
+	}
 		
  
 
